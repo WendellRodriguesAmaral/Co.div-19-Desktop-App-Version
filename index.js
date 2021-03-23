630ef8c27b2fb2c7b8c9b6d542119f18097602db
Wendell Rodrigues Amaral <wendellrodrigues97@hotmail.com>
2021-03-22 22:40:23 -0300

    the latest changes

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
+// import axios from 'axios';
+const container =  document.querySelector("main.all");
+
 const url = `https://covid-api.mmediagroup.fr/v1/cases?country=Brazil`
-const information = ` 
-    <section class="countryInformation">
-    <h2>Brasil<span>209.288.278 habitantes</span></h2>
-    <div class="boxes">
-    <div class="boxInformation boxConfirmed">
-        <p class="titleInformation">Confirmados</p>
-        <p class="information">000000</p>
-    </div>
-    <div class="boxInformation boxRecovered">
-        <p class="titleInformation">Recuperados</p>
-        <p class="information">000000</p>
-    </div>
-    <div class="boxInformation boxDeaths">
-        <p class="titleInformation">Mortes</p>
-        <p class="information">000000</p>
-    </div>
-    </div>        
-    </section>
-`
 
 const request = async function(url){
     const response = await fetch(url)
     const responseJson = await response.json()
     const responseAsArray = Object.entries(
-        responseJson) 
-    return responseAsArray //isso aq esta retornando uma promisse
+        responseJson)    
+    injectData (responseAsArray)       
 }
 
-request(url).then(value => {
-    console.log(value)}
-    )
+function formatNumbers(number, 
+    population = ""){
+    if(number === undefined) return ""
+    const num = number.toString().replace(/(\d{1,3})(\d{3})(\d{3})/ , "$1.$2.$3")
+                                    .replace(/(\d{1,3})(\d{3})/ ,"$1.$2")
+    return `${num} ${population}`
+}
 
+function formatName(name){
+    return name === "All" ? name = "Brasil" : name
+}
+
+function injectData(data){
+    data.forEach((location)=>{
+        const information = ` 
+            <section class="countryInformation">
+            <h2>${formatName(location[0])}<span>${formatNumbers(location[1].population, "habitantes")}</span></h2>
+            <div class="boxes">
+            <div class="boxInformation boxConfirmed">
+                <p class="titleInformation">Confirmados</p>
+                <p class="information">${formatNumbers(location[1].confirmed)}</p>
+            </div>
+            <div class="boxInformation boxRecovered">
+                <p class="titleInformation">Recuperados</p>
+                <p class="information">${formatNumbers(location[1].recovered)}</p>
+            </div>
+            <div class="boxInformation boxDeaths">
+                <p class="titleInformation">Mortes</p>
+                <p class="information">${formatNumbers(location[1].deaths)}</p>
+            </div>
+            </div>        
+            </section>
+        `
+        container.innerHTML += information
+    })
+}
+request(url)
+setInterval(request , 30 * 60000)
 
